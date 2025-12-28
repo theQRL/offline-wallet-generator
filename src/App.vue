8f8941ec6c238812a5978cbc59ad38519032d0a5
JP Lomas <jp@theqrl.org>
2025-12-28 21:18:57 +0000

    fix: refactor qrllib status to footer

--- src/App.vue
+++ src/App.vue
@@ -23,22 +23,40 @@
     <div class="h-20 bg-base-300"></div>
 
     <footer class="footer footer-center p-4 bg-neutral text-neutral-content border-t border-black/40 shadow-[0_-5px_5px_-5px_rgba(0,0,0,0.22)]">
-      <div>
+      <div class="flex items-center gap-4">
         <img class="h-14" :src="logoSvg" alt="QRL Logo">
+        <div v-if="qrllibLoaded" class="text-left">
+          <p class="text-sm text-neutral-content/80"><font-awesome-icon icon="check" class="text-success mr-1" />QRL Library loaded</p>
+          <p class="text-xs text-neutral-content/60">qrllib v1.2.4</p>
+        </div>
       </div>
     </footer>
   </div>
 </template>
 
 <script>
+/* global QRLLIB */
 import logoSvgRaw from '/logo.svg?raw';
 
 export default {
   name: 'App',
   data() {
     return {
+      qrllibLoaded: false,
       logoSvg: 'data:image/svg+xml;base64,' + btoa(logoSvgRaw)
     };
+  },
+  mounted() {
+    this.checkQRLLIB();
+  },
+  methods: {
+    checkQRLLIB() {
+      if (typeof QRLLIB !== 'undefined' && typeof QRLLIB.str2bin === 'function') {
+        this.qrllibLoaded = true;
+      } else {
+        setTimeout(() => this.checkQRLLIB(), 100);
+      }
+    }
   }
 };
 </script>
